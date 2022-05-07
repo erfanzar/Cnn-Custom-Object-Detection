3504dcae32759a4f32aa85f109eb6537a136cb4c
Erano- <59269023+erfanzar@users.noreply.github.com>
2022-05-07 12:28:16 +0430

    Update common.py

--- common.py
+++ common.py
@@ -1,96 +1,183 @@
 import torch.nn as nn
+import torch 
+import torch.optim as optim 
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torchvision import transforms
+from torch.nn import Conv2d , Linear , Dropout2d , BatchNorm2d , MaxPool2d , L1Loss , MSELoss , SiLU , Dropout , init , ReLU , CrossEntropyLoss ,Softmax , Upsample , LeakyReLU
+import time
+from torch.nn import Sequential
 
-from autopad import *
+class PredictionNeurons(nn.Module):
+  def __init__(self,num_in,num_classes , act=True):
+    super().__init__()
 
+    self.act = act
 
-class CNN(nn.Module):
+    self.num_in = int(num_in)
+    
+    self.num_classes = num_classes
 
-    def __init__(self,c1,c2,act=True,**kwargs):
+    self.dromp_input = 1
+
+    self.LeakyReLU1 = LeakyReLU(0.02).to(device)
+
+    self.LeakyReLU2 = LeakyReLU(0.02).to(device)
+
+    self.LeakyReLU3 = LeakyReLU(0.02).to(device)
+
+    self.T_num = 300
+    
+    self.fc0 = Linear(self.T_num,self.T_num//8).to(device)
+    
+    self.fc1 = Linear(self.T_num//8 , self.num_classes).to(device)
+    
+    self.ReLU = ReLU().to(device)
+
+    self.softmax = nn.Softmax()
+
+  def forward(self,x):
+    x1 = x[0][self.num_classes+5]
+    x2 = x[0][self.num_classes*2+5]
+    print('x1 shape {} x2 shape {}'.format(x1.shape , x2.shape))
+    x = torch.cat((x1,x2),1)
+    x = x.view(1,-1)
+
+    print(x.shape)
+
+    return self.ReLU(self.fc1(self.LeakyReLU1(self.fc0(x))))
+
+class NeuronBlock(nn.Module):
+  def __init__(self,num_in,num_classes , act=True):
+    super(NeuronBlock,self).__init__()
+
+    self.act = act
+    self.num_in = int(num_in)
+    self.num_classes = num_classes
+
+    self.T_num = 410
+    
+    self.dropout1 = Dropout(0.001).to(device)
+
+    self.LeakyReLU1 = LeakyReLU(0.02).to(device)
+
+    self.LeakyReLU2 = LeakyReLU(0.02).to(device)
+
+    self.fc0 = Linear(self.T_num,self.T_num//9).to(device)
+
+    self.ReLU = ReLU().to(device)
+
+    self.init_Weights()
+
+  
+  def forward(self,x):
+
+    x = x.view(-1,x.size)
+    
+    return self.ReLU(self.dropout1(self.LeakyReLU1(self.neuron1(x))))
         
-        super().__init__()
+  
+  def init_Weights(self):
 
-        self.conv = nn.Conv2d(c1,c2,bias= True if act == True else False , **kwargs)
-
-
-        self.batch = nn.BatchNorm2d(c2)
-
-        
-        self.activation = nn.SiLU() if act == True else nn.Identity()
+    if self.act == True:
+      torch.nn.init.kaiming_normal(self.neuron1).to(device)
+      torch.nn.init.kaiming_normal(self.neuron2).to(device)
+      torch.nn.init.kaiming_normal(self.neuron3).to(device)
 
 
-        self.act = act
+    
+    
+
+class BConvBlock(nn.Module):
+  def __init__(self,c_in,c_out,**kwargs):
+    
+    super(BConvBlock,self).__init__()
+    self.conv = None
+    self.c_in = c_in
+    self.c_out = c_out
+    
+  def forward(self,x):
+    self.conv = Sequential(
+        ConvBlock(self.c_in,self.c_out,kernel_size=6,stride=1),
+        ConvBlock(self.c_out,self.c_out//2,kernel_size=6,stride=1),
+    )
+    return self.maxpool(self.conv(x))
+
+class ConvBlock(nn.Module):
+  def __init__(self,c_in,c_out,act=False,**kwargs):
+    super(ConvBlock,self).__init__()
+    self.act = act
+    self.c_in = c_in
+    self.c_out = c_out
+    self.conv = Conv2d(self.c_in,self.c_out,bias=not act,**kwargs).to(device)
+    self.batch = BatchNorm2d(self.c_out).to(device)
+    self.SiLU = SiLU().to(device)
+
+  def forward(self,x):
+    if self.act==True:
+      return self.SiLU(self.batch(self.conv(x)))
+    else:
+      return self.conv(x)
 
 
-    def forward(self , x):
+class ResidualBlock(nn.Module):
+  def __init__(self,c_in,use_residual=True,num_repeat = 1,**kwargs):
+    super().__init__()
+    self.c_in = c_in
+    self.num_repeat = num_repeat
+    self.use_residual = use_residual
+    self.layer = nn.ModuleList()
+    for repeat in range(num_repeat):
+      self.layer += [
+          Sequential(
+            ConvBlock(c_in,c_in//2,kernel_size=1),
+            ConvBlock(c_in//2,c_in,kernel_size=3, padding=1)
+          )
+        ]
+  def forward(self,x):
+    for layer in self.layer:
 
-        if self.act :
-            
-            return self.activation(self.batch(self.conv(x)))
+      x = layer(x) + x if self.use_residual else layer(x)
 
-        else :
-
-            return self.conv(x)
-
-
-
-
-class Residual(nn.Module):
-
-    def __init__(self , c , use=True , num=1):
-
-        super().__init__()
-
-        self.layers = nn.ModuleList()
-
-        for repeat in range(num) :
-
-            self.layers += [
-                nn.Sequential(
-                    CNN(c , c * 2 , kernel_size=1),
-                    CNN(c * 2 , c , kernel_size=3 , padding=1)
-                )
-            ] 
-
-
-        self.num = num
-
-        self.use = use
-
-
-    def forward(self , x):
-
-        for layer in self.layers :
-
-
-            x = layer(x) + x if self.use == True else layer(x)
-
-
-        return x
-
-
-
+    return x
 
 
 class ScalePrediction(nn.Module):
+  def __init__(self,c_in,num_classes,**kwargs):
+    super().__init__()
+    self.c_in = c_in
+    self.num_classes = num_classes
+    self.pred = Sequential(
+
+        ConvBlock(c_in,c_in*2,kernel_size=3,padding=1,**kwargs),
+        ConvBlock(2*c_in,(num_classes+5)*3,False,kernel_size=1)#why plus 5 ? cause we want to also predict the x y h w and probeblity of theres an object in frame or no [po,x,y,w,h]
+    )
+  def forward(self,x):
+
+    """ 
+      N , 3,13,13,num_classes +5
+      N is for number of Batchs
+      3 is for anchor boxes
+      13 is the shape of image in width
+      13 is the shape of image in height
+      num_classes + 5 cause we want to have each class output + x,y,h,w,po
+
+    """
     
-    def __init__(self ,c ,num):
-       
-        super().__init__()
+    # return (
+    #       self.pred(x)
+    #       .reshape(x.shape[0], 3, self.num_classes + 5, x.shape[2], x.shape[3])
+    #       .permute(0, 1, 3, 4, 2)
+    #   )
 
-        self.pred = nn.Sequential(
-            CNN(c , c*2 , kernel_size=3 , padding=1),
-            CNN(c*2 ,3 * (num+5), act=False , kernel_size=1)
-        
-        )  
+    x = self.pred(x)
+    print(f'[INFO] being Reshape : {x.shape}')
+    x = x.reshape(x.shape[0],3,(self.num_classes +5),x.shape[2],x.shape[3])
+    print(f'[INFO] after Reshape : {x.shape}')
+    x = x.permute(0,1,3,4,2)
+    print(f'[INFO] after permute : {x.shape}')
+    return x
 
-        self.num = num
-
-        self.c = c
-
-    def forward(self , x):
-
-        return (
-            self.pred(x)
-            .reshape(x.shape[0] , 3 , self.num + 5 , x.shape[2] , x.shape[3])
-            .permute(0,1,3,4 , 2)
-        )
